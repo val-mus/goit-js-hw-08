760a2d8c48f6718f3b200a8a779eafbc707ef13f
Valentyn Musiienko <musiienkovalentyn@gmail.com>
2022-10-26 13:41:51 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,9 +5,11 @@ const refs = {
   textareaInput: document.querySelector('.feedback-form textarea'),
 };
 
+populateForm();
+
+
 const userData = {};
 
-populateForm();
 
 refs.form.addEventListener('submit', onFormSubmit);
 
@@ -16,12 +18,12 @@ refs.form.addEventListener('input', throttle(onInput, 500));
 function onFormSubmit(e) {
   e.preventDefault();
   console.log(userData);
-  refs.textareaInput.textContent = '';
-  e.target.reset();
+  e.currentTarget.reset();
   localStorage.removeItem('feedback-form-state');
 }
 
 function onInput(e) {
+  
   userData[e.target.name] = e.target.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(userData));
 }
@@ -29,8 +31,7 @@ function onInput(e) {
 function populateForm() {
   const userInput = localStorage.getItem('feedback-form-state');
   if (userInput) {
-    refs.emailInput.textContent = JSON.parse(userInput).email;
-
-    refs.textareaInput.textContent = JSON.parse(userInput).message;
+    refs.emailInput.value = JSON.parse(userInput).email;
+    refs.textareaInput.value = JSON.parse(userInput).message;
   }
 }
